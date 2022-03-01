327907f5859ac32f3ecb78a2ef0d8eb1fb888a9b
maomao9-0 <tiashiwei04@gmail.com>
2022-03-01 22:43:55 +0800

    Added tags in Problem.py

--- Problem.py
+++ Problem.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 class Problem:
     def __init__(self, path):
         self.content = ''
+        self.tags = []
         html = ''
         with open(path, 'rb') as file:
             html = file.read()
@@ -13,14 +14,21 @@ class Problem:
         self.get_content(soup)
         self.get_tags(soup)
 
+    def __str__(self):
+        return self.content + '\n' + str(self.tags)
+
     def get_content(self, soup):
         str = ' '.join(soup.find('div', class_='problem-statement').
                        find_all('div', recursive=False)[1].stripped_strings)
         self.content = ' '.join(''.join([i if i.isalnum() or i.isspace() else '' for i in str]).split())
 
     def get_tags(self, soup):
+        spans = soup.find(id='sidebar').find_all('span', class_='tag-box')
+        for span in spans:
+            self.tags.append(span.text.strip())
         pass
 
 
 if __name__ == '__main__':
     problem = Problem('problems/1644F.html')
+    print(problem)
